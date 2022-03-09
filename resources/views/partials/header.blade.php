45a737f9bc09d599abb8cc2fc78d2cb282f15338
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-09 21:51:44 +0100

    Header completato

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,32 @@
+<header>
+    <div class="container">
+        <section>
+            <div>DC POWER&trade; VISA&reg;</div>
+            <div>ADDITIONAL DC SITES &#9660;</div>
+        </section>
+        <section>
+            <div class="logo">
+                <a href="#">
+                    <img src="{{ asset('images/dc-logo.png') }}" alt="DC">
+                </a>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Collectibles</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">Fans</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Shop</a></li>
+                </ul>
+
+                <input type="text" placeholder="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </nav>
+        </section>
+    </div>
+</header>
