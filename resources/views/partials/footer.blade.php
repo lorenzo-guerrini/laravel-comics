d7de4d606f488cd96a6ab802515c19965253be52
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-09 22:19:10 +0100

    Finito footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,114 @@
+<footer>
+    <section>
+        <div class="container">
+            <div class="footer-top-item">
+                <a href="#">
+                    <img src="{{ asset('images/buy-comics-digital-comics.png') }}" alt="Digital Comics" />
+                    <span class="footer-top-item-name">Digital Comics</span>
+                </a>
+            </div>
+            <div class="footer-top-item">
+                <a href="#">
+                    <img src="{{ asset('images/buy-comics-merchandise.png') }}" alt="DC Merchandise" />
+                    <span class="footer-top-item-name">DC Merchandise</span>
+                </a>
+            </div>
+            <div class="footer-top-item">
+                <a href="#">
+                    <img src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="Subscrition" />
+                    <span class="footer-top-item-name">Subscriptions</span>
+                </a>
+            </div>
+            <div class="footer-top-item">
+                <a href="#">
+                    <img src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="Comic Shop Locator" />
+                    <span class="footer-top-item-name">Comic Shop Locator</span>
+                </a>
+            </div>
+            <div class="footer-top-item">
+                <a href="#">
+                    <img src="{{ asset('images/buy-dc-power-visa.svg') }}" alt="DC Power Visa" />
+                    <span class="footer-top-item-name">DC Power Visa</span>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="category">
+                <h3>DC Comics</h3>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+            </div>
+
+            <div class="category">
+                <h3>DC Comics</h3>
+                <ul>
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+            </div>
+
+            <div class="category">
+                <h3>DC</h3>
+                <ul>
+                    <li><a href="#">Terms Of Use</a></li>
+                    <li><a href="#">Privacy Policy(New)</a></li>
+                    <li><a href="#">Ad Choices</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscriptions</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificates</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+
+            <div class="category">
+                <h3>DC Comics</h3>
+                <ul>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">DC Universe</a></li>
+                    <li><a href="#">DC Power Visa</a></li>
+                </ul>
+            </div>
+            <div>All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>.
+                All rights reserved.<br><a href="#">Cookies Settings</a></div>
+        </div>
+
+        <div id="dc-logo-bg">
+            <img src="{{ asset('images/dc-logo-bg.png') }}" alt="DC">
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div>
+                <button><a href="#">Sign-up now!</a></button>
+            </div>
+
+            <div class="footer-bottom-dx">
+                <span>Follow us</span>
+                <ul>
+                    <li><a href="#"><img src="{{ asset('images/footer-facebook.png') }}" alt="Facebook" /></a></li>
+                    <li><a href="#"><img src="{{ asset('images/footer-twitter.png') }}" alt="Twitter" /></a></li>
+                    <li><a href="#"><img src="{{ asset('images/footer-youtube.png') }}" alt="YouTube" /></a></li>
+                    <li><a href="#"><img src="{{ asset('images/footer-pinterest.png') }}" alt="Pinterest" /></a></li>
+                    <li><a href="#"><img src="{{ asset('images/footer-periscope.png') }}" alt="Periscope" /></a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+</footer>
