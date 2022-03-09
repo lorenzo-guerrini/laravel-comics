23d32f9fb4a0cdd6d8c45d46abfcd85b384df0f8
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-09 21:25:56 +0100

    comics.blade completato

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,5 +3,27 @@
 @section('page-title', 'Comics')
 
 @section('content')
-    
+    <section>
+        <div class="jumbotron"></div>
+    </section>
+    <section>
+        <div class="container">
+            <h2>Current Series</h2>
+            <div class="series-container">
+                @foreach ($comics as $key => $value)
+                    <div class="comic">
+                        <a href="#">
+                            <div class="comic-thumb">
+                                <img src="{{ $value['thumb'] }}" alt="{{ $value['title'] }}">
+                            </div>
+                            <div class="comic title">
+                                {{ $value['title'] }}
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+            <button>Load More</button>
+        </div>
+    </section>
 @endsection
