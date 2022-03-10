ecb587081efa5ca0cc3168a9c594b42bdeadbf73
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-10 11:00:06 +0100

    Footer completato

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -40,56 +40,63 @@
 
     <section class="footer-middle">
         <div class="container">
-            <div class="category">
-                <h3>DC Comics</h3>
-                <ul>
-                    <li><a href="#">Characters</a></li>
-                    <li><a href="#">Comics</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">News</a></li>
-                </ul>
+            <div class="sx">
+                <div class="categories">
+                    <div class="category">
+                        <h3>DC Comics</h3>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="category">
+                        <h3>DC Comics</h3>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="category">
+                        <h3>DC</h3>
+                        <ul>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy Policy(New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="category">
+                        <h3>DC Comics</h3>
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="copyright">
+                    All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>.
+                    All rights reserved.<br><a href="#">Cookies Settings</a>
+                </div>
             </div>
 
-            <div class="category">
-                <h3>DC Comics</h3>
-                <ul>
-                    <li><a href="#">Shop DC</a></li>
-                    <li><a href="#">Shop DC Collectibles</a></li>
-                </ul>
-            </div>
-
-            <div class="category">
-                <h3>DC</h3>
-                <ul>
-                    <li><a href="#">Terms Of Use</a></li>
-                    <li><a href="#">Privacy Policy(New)</a></li>
-                    <li><a href="#">Ad Choices</a></li>
-                    <li><a href="#">Advertising</a></li>
-                    <li><a href="#">Jobs</a></li>
-                    <li><a href="#">Subscriptions</a></li>
-                    <li><a href="#">Talent Workshops</a></li>
-                    <li><a href="#">CPSC Certificates</a></li>
-                    <li><a href="#">Ratings</a></li>
-                    <li><a href="#">Shop Help</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
-            </div>
-
-            <div class="category">
-                <h3>DC Comics</h3>
-                <ul>
-                    <li><a href="#">DC</a></li>
-                    <li><a href="#">MAD Magazine</a></li>
-                    <li><a href="#">DC Kids</a></li>
-                    <li><a href="#">DC Universe</a></li>
-                    <li><a href="#">DC Power Visa</a></li>
-                </ul>
-            </div>
-            {{-- <div>All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>.
-                All rights reserved.<br><a href="#">Cookies Settings</a></div> --}}
             <div id="dc-logo-bg">
                 <img src="{{ asset('images/dc-logo-bg.png') }}" alt="DC">
             </div>
